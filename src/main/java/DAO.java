1946da6a35eae3a73a4f4d4cbfc9d0c07423ac32
ZAOUAM Sirageddine <zaouam.sirageddine@hotmail.com>
2020-04-10 17:47:51 +0200

    La classe abstraite DAO et la fabrique

---
+++ src/main/java/DAO.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+
+public abstract class DAO<T> {
+
+	public Connection connect = ConnectionPostgreSQL.getInstance();
+	
+	/**
+	 * Permet de récupérer un objet via son ID
+	 * @param id
+	 * @return
+	 */
+	public abstract T find(long id);
+	
+	/**
+	 * Permet de créer une entrée dans la base de données
+	 * par rapport à un objet
+	 * @param obj
+	 */
+	public abstract T create(T obj);
+	
+	/**
+	 * Permet de mettre à jour les données d'une entrée dans la base 
+	 * @param obj
+	 */
+	public abstract T update(T obj);
+	
+	/**
+	 * Permet la suppression d'une entrée de la base
+	 * @param obj
+	 */
+	public abstract void delete(T obj);
+}
---
+++ src/main/java/DAOFactory.java
@@ -0,0 +1,10 @@
+/*
+ * Une Fabrique pour les DAOs
+ */
+
+public class DAOFactory{
+	//public static DAO<Personnel> getPersonnelDAO(){
+		//return new PersonnelDAO();
+		
+	//}
+}
